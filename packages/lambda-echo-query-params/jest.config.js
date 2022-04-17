7d11b16d4820074282a2bc15aef2fa04212915bc
Vishal Kumar <connectwithvkumar@gmail.com>
2022-04-17 14:22:12 -0400

    deploy to aws lambda

---
+++ packages/lambda-echo-query-params/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'lambda-echo-query-params',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/lambda-echo-query-params',
+};
